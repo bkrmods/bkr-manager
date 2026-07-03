6efee85d7dd40d2cf1ba02abfcc07546b6279397
bkrmods <bkrmods@gmail.com>
2026-07-04 00:10:45 +0200

    Sprint 8.6 - Remove fake dashboard data

--- lib/monthly-goal.ts
+++
@@ -1,4 +0,0 @@
-export const monthlyGoal = {
-  current: 8500,
-  target: 10000,
-};
--- lib/recent-activity.ts
+++
@@ -1,6 +0,0 @@
-export const recentActivity = [
-  "Commande #1042 créée",
-  "Paiement reçu de Lucas Martin",
-  "Stock GMT Pepsi mis à jour",
-  "Nouveau client ajouté",
-];
